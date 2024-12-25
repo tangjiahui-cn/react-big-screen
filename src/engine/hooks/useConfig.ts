9ea760e3df2e7071e6208a7a18918a7f204f577e
tangjiahui <tjhaaa@163.com>
2024-12-25 14:14:21 +0800

    feat(.): user-select none

--- src/engine/hooks/useConfig.ts
+++ src/engine/hooks/useConfig.ts
@@ -1,11 +1,11 @@
-import { useGlobalSelector } from '@/engine';
-
 /**
  * useConfig
  *
  * @author tangjiahui
  * @date 2024/12/24
  */
+import { useGlobalSelector } from '@/engine';
+
 export function useConfig() {
   return useGlobalSelector((state) => state.config);
 }
