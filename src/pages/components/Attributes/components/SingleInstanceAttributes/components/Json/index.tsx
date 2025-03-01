c83f7079ea19454c784813343ddbb33cdccd8701
tangjiahui <tjhaaa@163.com>
2025-03-01 23:39:20 +0800

    feat(.): 优化json读取

--- src/pages/components/Attributes/components/SingleInstanceAttributes/components/Json/index.tsx
+++ src/pages/components/Attributes/components/SingleInstanceAttributes/components/Json/index.tsx
@@ -20,7 +20,7 @@ export default function () {
 
   function handleReset() {
     form.setFieldsValue({
-      code: JSON.stringify(componentNode || {}, null, "\t"),
+      code: componentNode ? JSON.stringify(componentNode || {}, null, "\t") : "",
     });
   }
 
@@ -41,8 +41,9 @@ export default function () {
   }
 
   useEffect(() => {
+    // 当选中不同组件时，才会初始读取一次 componentNode 对象。
     handleReset();
-  }, [componentNode]);
+  }, [componentNode?.id]);
 
   return (
     <Form className={styles.json} form={form}>
