f4557bf41b55e9c3ef631036a3c0f6b76f16ff1e
tangjiahui <tjhaaa@163.com>
2025-03-03 15:58:43 +0800

    build(.): 手动分包chunks

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,11 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { createHtmlPlugin } from "vite-plugin-html";
 import pkg from "./package.json";
+import { analyzer } from "vite-bundle-analyzer";
 const dayjs = require("dayjs");
+
+// 是否分析打包情况
+const isAnalyzer = process.env.mode === "analyzer";
 
 // 是否构建为github部署页面
 const isDeployGithub = process.env.deploy === "github";
@@ -31,6 +35,19 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          antd: ["antd"], // 单独分antd，避免按需使用antd，导致多包组合大体积产物chunk变化，致使node_modules chunk缓存失效。
+          lodash: ["lodash-es"], // 单独分lodash（同antd）
+          ahooks: ["ahooks"], // 单独分ahooks（同antd）
+          "monaco-editor": ["monaco-editor"],
+          echarts: ["echarts"],
+        },
+      },
+    },
+  },
   plugins: [
     react(),
     createHtmlPlugin({
@@ -52,5 +69,6 @@ export default defineConfig({
         },
       ],
     }),
+    isAnalyzer && analyzer(),
   ],
 });
