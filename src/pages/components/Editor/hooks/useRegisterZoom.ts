b20054a6a69eb3f5059aab2f891fcfe7101bbab0
tangjiahui <tjhaaa@163.com>
2025-03-26 17:13:48 +0800

    feat(wheel): 同时兼容WheelEvent的deltaY和wheelDeltaY属性。

--- src/pages/components/Editor/hooks/useRegisterZoom.ts
+++ src/pages/components/Editor/hooks/useRegisterZoom.ts
@@ -15,6 +15,35 @@ import {
 import { RefObject } from "react";
 import { isClickMouseMid } from "@/utils";
 
+interface WheelInfo {
+  direction: 1 | -1 | 0; // 1正向 0无 -1负向
+  times: number; // 每次变化多少次的比例缩放
+}
+
+// 获取滚轮信息
+function getWheelInfo(e: WheelEvent): WheelInfo {
+  let times: WheelInfo["times"] = 1;
+  let direction: WheelInfo["direction"] = 0;
+
+  // 兼容 wheelDeltaY
+  if ((e as any).wheelDeltaY) {
+    const wheelDeltaY = (e as any).wheelDeltaY;
+    times = Math.abs(wheelDeltaY) / 120;
+    direction = wheelDeltaY > 0 ? 1 : wheelDeltaY < 0 ? -1 : 0;
+  } else if ((e as any).deltaY) {
+    const deltaY = (e as any).deltaY;
+    // 计算方式：times = (deltaY - 4) / 40
+    // 以40像素变化为一次缩放变化，使其deltaY的times，和wheelDeltaY的times一致
+    times = Math.floor(Math.max(Math.abs(deltaY) - 4, 0) / 40) || 1;
+    direction = deltaY > 0 ? 1 : deltaY < 0 ? -1 : 0;
+  }
+
+  return {
+    times,
+    direction,
+  };
+}
+
 export function useRegisterZoom(domRef: RefObject<HTMLElement>) {
   useDomEvents(domRef, {
     // 重置
@@ -29,16 +58,13 @@ export function useRegisterZoom(domRef: RefObject<HTMLElement>) {
       if (isKeyPressed("ctrl")) {
         // 阻止滚动条滚动
         e.preventDefault();
-        // 鼠标滚动偏移量（正值往上，负值往下）
-        const wheelDeltaY = (e as any).wheelDeltaY;
-        // 缩放倍数（用于实现鼠标加速滚动，缩放同样更快）
-        const times = Math.abs(wheelDeltaY) / 120;
+        const { direction, times } = getWheelInfo(e as WheelEvent);
         // 缩小比例
-        if (wheelDeltaY < 0) {
+        if (direction < 0) {
           zoomEditorSmaller(times);
         }
         // 放大比例
-        if (wheelDeltaY > 0) {
+        if (direction > 0) {
           zoomEditorLarger(times);
         }
       }
