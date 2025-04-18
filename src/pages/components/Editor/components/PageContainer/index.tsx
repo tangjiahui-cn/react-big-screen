012fc6d3d0594de9aeac4fbfebe9d587bb442f90
tangjiahui <tjhaaa@163.com>
2025-04-18 16:41:48 +0800

    fix(.): 修复预览页面显示不正确的bug

--- src/pages/components/Editor/components/PageContainer/index.tsx
+++ src/pages/components/Editor/components/PageContainer/index.tsx
@@ -10,6 +10,7 @@ import InfiniteContainer from "@/packages/infiniteContainer";
 import { zoomEditorDefault } from "@/packages/shortCutKeys";
 
 interface Props {
+  preview?: boolean; // 是否预览模式
   style?: React.CSSProperties;
   className?: string;
   children?: React.ReactNode;
@@ -18,6 +19,27 @@ interface Props {
 export default React.forwardRef((props: Props, ref: ForwardedRef<HTMLDivElement>) => {
   const { options } = useCurrentPage() || {};
   const config = useConfig();
+
+  const children = (
+    <div
+      ref={ref}
+      className={props?.className}
+      style={{
+        background: options?.background,
+        border: options?.bordered ? `1px solid ${options.borderColor}` : undefined,
+        width: config.width,
+        height: config.height,
+        ...props?.style,
+      }}
+    >
+      {props?.children}
+    </div>
+  );
+
+  if (props?.preview) {
+    return children;
+  }
+
   return (
     <InfiniteContainer
       style={{ width: "100%", height: "100%" }}
@@ -39,19 +61,7 @@ export default React.forwardRef((props: Props, ref: ForwardedRef<HTMLDivElement>
         zoomEditorDefault();
       }}
     >
-      <div
-        ref={ref}
-        className={props?.className}
-        style={{
-          background: options?.background,
-          border: options?.bordered ? `1px solid ${options.borderColor}` : undefined,
-          width: config.width,
-          height: config.height,
-          ...props?.style,
-        }}
-      >
-        {props?.children}
-      </div>
+      {children}
     </InfiniteContainer>
   );
 });
