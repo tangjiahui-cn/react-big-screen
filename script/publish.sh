9ff1281844a1122193961b5ba66ce7bf0f2b01c9
tangjiahui <tjhaaa@163.com>
2025-10-17 13:48:46 +0800

    ci(.): 更新 publish.sh

--- script/publish.sh
+++ script/publish.sh
@@ -1,6 +1,7 @@
 # 自动发布脚本
 ## tips: ${}里面是替换字符串，$()里面是替换命令
 
+echo "--> step1：开始更新版本号"
 # 1、版本号更新
 ## 传入参数指定版本号
 if [ "$1" ];
@@ -14,39 +15,37 @@ then
   VERSION=$(node -p "require('./package.json').version")
 fi
 NAME=$(node -p "require('./package.json').name")
-echo "--> step1"
 
 # 2、提交git记录
+echo "-> step2：提交git记录"
 git commit -am "docs(.): publish version ${VERSION}"
-echo "--> step2"
 
 # 3、标记tag
+echo "-> step3：标记tag"
 TAG_NAME=v${VERSION}
 git tag -d ${TAG_NAME} 2>/dev/null
 git tag ${TAG_NAME}
 echo step3
-echo "--> step3"
 
 # 4、打印消息
+echo "-> step4：打印消息"
 printf "\n\n"
 printf "【BUILD SUCCESS】\n"
 printf "Tag is ${TAG_NAME}.\n"
 printf "Version is ${VERSION}.\n"
 printf "\n\n"
-echo "--> step4"
 
 # 5、生成changelog
+echo "-> step5：生成changelog"
 conventional-changelog -p angular -i CHANGELOG.md -s -r 0
 git commit -am "docs(CHANGELOG.md): update changelog" # (新增提交，避免重写提交覆盖tag)
-echo "--> step5"
 
 # 发布库
+echo "-> step6：发布 https://registry.npmjs.org/"
 ADDRESS=https://registry.npmjs.org/
 npm unpublish ${NAME}@${VERSION} 2>/dev/null --registry=${ADDRESS}
 npm publish --registry=${ADDRESS}
-echo "--> step6"
 
+echo "-> step7:推送远程git分支"
 git push origin
 git push origin --tags
-
-echo "--> end"
